92b6d55ab29f666f94855b36b2add876c7c872a0
Tirumala R Marri <mtreddy@gmail.com>
2017-11-16 17:06:33 -0800

    Adding gradient descent code

---
+++ ml/gd.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class GradientDescentLinearRegression:
+    def __init__(self, learning_rate=0.01, iterations=1000):
+        self.learning_rate, self.iterations = learning_rate, iterations
+
+    def fit(self, X, y):
+        b = 0
+        m = 5
+        n = X.shape[0]
+        for _ in range(self.iterations):
+            b_gradient = -2 * np.sum(y - m*X + b) / n
+            m_gradient = -2 * np.sum(X*(y - (m*X + b))) / n
+            b = b + (self.learning_rate * b_gradient)
+            m = m - (self.learning_rate * m_gradient)
+        self.m, self.b = m, b
+
+    def predict(self, X):
+        return self.m*X + self.b
+
+np.random.seed(42)
+X = np.array(sorted(list(range(5))*20)) + np.random.normal(size=100, scale=0.5)
+y = np.array(sorted(list(range(5))*20)) + np.random.normal(size=100, scale=0.25)
+plt.style.use('fivethirtyeight')
+
+clf = GradientDescentLinearRegression()
+clf.fit(X, y)
+plt.scatter(X, y, color='black')
+plt.plot(X, clf.predict(X))
+plt.gca().set_title("Gradient Descent Linear Regressor")
+plt.show()
+
