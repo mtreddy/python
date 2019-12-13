5ecdfca33f5208dbdf91cd418f44f02a07b5c71f
Tirumala Marri <tirmarri@amd.com>
2019-12-13 14:54:34 -0600

    rmt_acc file changes

--- gen/rmt_acc.py
+++ gen/rmt_acc.py
@@ -5,6 +5,15 @@ import sys
 if len(sys.argv) < 4:
   print("rmt ip-addr user-id- passwd")
 
+
+def get_cpu_info(s):
+    s.sendline('lscpu')
+    s.prompt()
+    line = s.before
+    print(line)
+    
+def get_no_of
+
 ip = sys.argv[1]
 uid = sys.argv[2]
 pasw = sys.argv[3]
@@ -20,3 +29,5 @@ try:
 except pxssh.ExceptionPxssh as e:
     print("pxssh failed to login")
     print(e)
+
+get_cpu_info(s)
