15eb112fec08436886c0aa0f4daa15818d7c5f55
Tirumala Marri <tirmarri@amd.com>
2020-01-16 14:29:39 -0800

    mine_specint.py changes

--- dmine/mine_specint.py
+++ dmine/mine_specint.py
@@ -10,30 +10,82 @@ from requests import get
 ## get specint schore?
 class minespec:
     def findIntScoreForId(self, lst, Id):
+        data = []
         pattrn = r'EPYC %s' % (Id)
         for itm in lst:
             if len(re.findall(pattrn,itm.text)) != 0:
                 tag = itm.find_all("td");
                 print("\n")
+                ddict = {}
                 for score in tag:
                     if score.attrs['class'][0] == "test_sponsor":
-                        print("Test Sponsor  :%s" % (score.text))
+                        ddict['Test Sponsor  :'] = score.text
+                        #print("Test Sponsor  :%s" % (score.text))
+                    if score.attrs['class'][0] == "hw_model":
+                        ddict['HW Model      :'] =  score.text
+                        #print("HW Model      :%s" % (score.text.split("\n")[0]))
                     if score.attrs['class'][0] == "basemean":
-                        print("basemean      :%s" % (re.findall(r'\d+',score.text)[0]))
-                    if score.attrs['class'][0] == "peakmean":
-                        print("peakmean      :%s" % (re.findall(r'\d+',score.text)[0]))
+                        ddict['Basemean      :'] = (re.findall(r'\d+',score.text)[0])
+                        #print("basemean      :%s" % (re.findall(r'\d+',score.text)[0]))
+                    if score.attrs['class'][0] == "peakmean" and (len(re.findall(r'\d+',score.text)) != 0):
+                        ddict['Peakmean      :'] = (re.findall(r'\d+',score.text)[0])
+                        #print("peakmean      :%s" % (re.findall(r'\d+',score.text)[0]))
                     if score.attrs['class'][0] == "base_copies":
-                        print("Copies        :%s" % (re.findall(r'\d+',score.text)[0]))
+                        ddict['Copeis        :'] = (re.findall(r'\d+',score.text)[0])
+                        #print("Copies        :%s" % (re.findall(r'\d+',score.text)[0]))
                     if score.attrs['class'][0] == "hw_ncores":
-                        print("No of Cores   :%s" % (re.findall(r'\d+',score.text)[0]))
+                        ddict['No of Cores   :'] = (re.findall(r'\d+',score.text)[0])
+                        #print("No of Cores   :%s" % (re.findall(r'\d+',score.text)[0]))
                     if score.attrs['class'][0] == "hw_nchips":
-                        print("No of Sockets :%s" % (re.findall(r'\d+',score.text)[0]))
+                        ddict['No of Sockets   :'] = (re.findall(r'\d+',score.text)[0])
+                        #print("No of Sockets :%s" % (re.findall(r'\d+',score.text)[0]))
+                data.append(ddict)
+        return data
+    ## Function which takes Vendor name as ibput and dups different part-ids and scores
+    def findIntForVendorId(self, lst, vId):
+        data = []
+        pattrn = r'%s' % (vId)
+        for itm in lst:
+            if (len(re.findall(r'EPYC',itm.text)) != 0) and (len(re.findall(pattrn,itm.text)) != 0):
+                tag = itm.find_all("td");
+                print("\n")
+                ddict = {}
+                for score in tag:
+                    if score.attrs['class'][0] == "test_sponsor":
+                        ddict['Test Sponsor  :'] = score.text
+                        #print("Test Sponsor  :%s" % (score.text))
+                    if score.attrs['class'][0] == "hw_model":
+                        ddict['HW Model      :'] =  score.text
+                        #print("HW Model      :%s" % (score.text.split("\n")[0]))
+                    if score.attrs['class'][0] == "basemean":
+                        ddict['Basemean      :'] = (re.findall(r'\d+',score.text)[0])
+                        #print("basemean      :%s" % (re.findall(r'\d+',score.text)[0]))
+                    if score.attrs['class'][0] == "peakmean" and (len(re.findall(r'\d+',score.text)) != 0):
+                        ddict['Peakmean      :'] = (re.findall(r'\d+',score.text)[0])
+                        #print("peakmean      :%s" % (re.findall(r'\d+',score.text)[0]))
+                    if score.attrs['class'][0] == "base_copies":
+                        ddict['Copeis        :'] = (re.findall(r'\d+',score.text)[0])
+                        #print("Copies        :%s" % (re.findall(r'\d+',score.text)[0]))
+                    if score.attrs['class'][0] == "hw_ncores":
+                        ddict['No of Cores   :'] = (re.findall(r'\d+',score.text)[0])
+                        #print("No of Cores   :%s" % (re.findall(r'\d+',score.text)[0]))
+                    if score.attrs['class'][0] == "hw_nchips":
+                        ddict['No of Sockets   :'] = (re.findall(r'\d+',score.text)[0])
+                        #print("No of Sockets :%s" % (re.findall(r'\d+',score.text)[0]))
+                data.append(ddict)
+        return data
+    ## Function to download SPECINT text file.
+    ## Criteria need to be unique, If it finds more than one result 
+    ## no result file will be downloaded
+    ## criteria = {'test_sponsor':'Dell', 'core_id':'7662', 'hw_chips': '2', 'base_copies':'128'}
+    def downLoadSPecIntTexctResults(self, lst, criteria, blink):
+        if len(criteria) == 0 or type(criteria) != dict:
+            print("Pl pass valid criteria \n EX:criteria = {'test_sponsor':'Dell', 'core_id':'7662', 'hw_chips': '2', 'base_copies':'128'} ")
 
 
-                     
-
-
-
+    ## Function to analyze SPECINT results
+    def analyzeSpectIntResults(self, txtFile):
+        print("Need to implement")
 
 ## This will need part number
 url = "https://www.spec.org/cpu2017/results/rint2017.html"
@@ -45,15 +97,7 @@ soup = BeautifulSoup(res.text, "html.parser")
 lst = soup.find_all("tr")
 print(len(lst))
 
-ref = lst[3].find_all("a")
 
-print(ref[2].attrs['href'])
-
-for itm in lst:
-     if len(re.findall(r'EPYC 7662',itm.text)) != 0:
-         aa = itm.find_all("td");
-         for a in aa:
-             if a.text == 'Text':
-                 print(a)
-
-
+specint = minespec()
+ids = specint.findIntScoreForId(lst,Id="7642")
+vids = specint.findIntForVendorId(lst,vId="ASUS")
