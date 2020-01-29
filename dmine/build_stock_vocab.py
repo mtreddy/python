64fcfb621eb10712e18dcb714adfa1ac25b18bd3
Tirumala Marri <tirmarri@amd.com>
2020-01-29 12:42:37 -0800

    Adding code to read text from pdf

---
+++ dmine/build_stock_vocab.py
@@ -0,0 +1,40 @@
+## To build vocabulary for stock market
+##
+
+
+import os
+import sys
+import csv
+import PyPDF2
+import tabula
+import nltk
+import pandas as pd
+import numpy as np
+
+
+class vocab:
+    def __init__(self):
+        print("Initing")
+
+    def vReadPdfTables(self, fpath):
+        pobj = open(fpath,'rb')
+        pdfrd = PyPDF2.PdfFileReader(pobj)
+        tables=tabula.read_pdf(pf, pages="all", multiple_tables=True)
+        return tables
+
+    def vReadPdfText(self, fpath):
+        txt = []
+        pobj = open(fpath,'rb')
+        pdfrd = PyPDF2.PdfFileReader(pobj)
+        for i in range(0, pdfrd.numPages): 
+          pg = pdfrd.getPage(i)
+          txt.append(pg.extractText())
+        return txt
+
+
+## main ##
+
+fpath = '/Users/tirmarri/Documents/technical/personal/investments/2020_picks.pdf'
+
+vc = vocab()
+txt = vc.vReadPdfText(fpath)
