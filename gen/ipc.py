1b7702bf2db3ab0682d8f8266bec2edaed523a4e
Tirumala Marri <tirmarri@amd.com>
2020-07-22 13:04:00 -0500

    IPC data collection

--- gen/ipc.py
+++ gen/ipc.py
@@ -6,22 +6,14 @@ import pandas as  pd
 print(sys.argv[1])
 dst = "./tmp.csv"
 shutil.copyfile(sys.argv[1], dst)
-pdata = pd.read_csv(dst, sep=',',skiprows=34)
-icols = [col for col in pdata.columns if 'CPI (Sys + User)' in col]
-#print(ser)
-#print(ser.sum())
-#print(len(pdata.columns))
-ncores =128
-#cores =int( (len(pdata.columns)-1)/17)
+#pdata = pd.read_csv(dst, sep=',',skiprows=34)
+pdata = pd.read_csv(dst, sep=',',skiprows=42)
+print(pdata.columns)
+icols = [col for col in pdata.columns if 'IPC (Sys + User)' in col]
 cores = len(icols)
 print("Number of cores =%d" % cores)
 print("Number of rows", len(pdata.index))
 for i in range(cores):
-	#print(pdata.columns[i*17+3])
-	#ser = pdata[pdata.columns[i*17+3]]
-	#avg = (ser.sum()/len(pdata.index))
 	ser = pdata[icols[i]]
-	#print(type(ser))
-	#print(ser)
 	avg = (ser.sum()/len(ser.index))
 	print("Core %d avg cpi=%f" % (i, avg))
