5b91d56fab0d50759b41dd6cadba9081be91f9d7
mtreddy <mtreddy@gmail.com>
2024-02-09 18:45:18 -0800

    Restructuring into class

--- math/crypto/rsa_pss.py
+++ math/crypto/rsa_pss.py
@@ -22,24 +22,62 @@ class rsa_pss_test:
         pub = pri.public_key()
         return (pri, pub)
 
-    def rsa_sign(self, msg, prkey, hash_type):
+    def rsa_sign_data(self, msg, prkey, hash_type):
         if hash_type == SHA256:
             ht = hashes.SHA256()
+        else:
+            print("Not supported")
+            return "Not supported"
         sig = prkey.sign( msg, padding.PSS(mgf=padding.MGF1(ht), 
             salt_length=padding.PSS.MAX_LENGTH), ht)
         return sig
 
-    def rsa_verify(self, pkey, sig, hash_type):
+    def rsa_verify_data(self, pkey, sig, hash_type):
         if hash_type == SHA256:
             ht = hashes.SHA256()
             msg1 = b"A message I want to sig"
+        else:
+            print("Not supported")
+            return "Not supported"
         try:
             out = pkey.verify(sig, self.msg, padding.PSS(mgf=padding.MGF1(ht),
                 salt_length=padding.PSS.MAX_LENGTH), ht)
+            if out == None:
+                out = "Passed"
         except Exception as e:
             out = "Failed"
             print("Signature check failed")
         return out
+
+    def rsa_sign_digest(self, dgst, prkey, ht):
+        sig = prkey.sign( dgst, padding.PSS(mgf=padding.MGF1(ht), 
+            salt_length=padding.PSS.MAX_LENGTH), ht)
+        return sig
+
+    def rsa_verify_digest(self, dgst, sig, pkey, ht):
+        try:
+            out = pkey.verify(sig, dgst, padding.PSS(mgf=padding.MGF1(ht),
+                salt_length=padding.PSS.MAX_LENGTH), ht)
+            if out == None:
+                out = "Passed"
+        except Exception as e:
+            out = "Failed"
+            print("Signature check failed")
+        return out
+
+    def rsa_get_digest(self, hash_type):
+        if hash_type  == SHA256:
+            ht = hashes.SHA256()
+        else:
+            print("Not supported")
+            return "Not supported"
+
+        hashit = hashes.Hash(ht)
+        print(self.msg)
+        hashit.update(self.msg)
+        dgst = hashit.finalize()
+        return (dgst, ht)
+
 
 
 
@@ -51,9 +89,13 @@ message = b"A message I want to sign"
 #print(out)
 rverify = rsa_pss_test(message)
 (pri, pub) = rverify.rsa_key_gen(RSA2048, EXP65537)
-sig = rverify.rsa_sign(message, pri, SHA256)
-print("signature",sig)
-print("pubkey", pub)
-print("sha", sha)
-out = rverify.rsa_verify(pub, sig, SHA256)
+sig = rverify.rsa_sign_data(message, pri, SHA256)
+#print("signature",sig)
+out = rverify.rsa_verify_data(pub, sig, SHA256)
 print(out)
+(dgst, ht) = rverify.rsa_get_digest(SHA256)
+print("dgst", dgst)
+sig1 = rverify.rsa_sign_digest(dgst, pri, ht)
+#print("sig", sig)
+out = rverify.rsa_verify_digest(dgst, sig1, pub, ht)
+print("out", out)
