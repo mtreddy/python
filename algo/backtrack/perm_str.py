c454183c72a66058c6526db52b123907c3cb31de
mtreddy <mtreddy@gmail.com>
2024-02-19 17:45:26 -0800

    Adding permuations of a string with lower and upper cases

---
+++ algo/backtrack/perm_str.py
@@ -0,0 +1,39 @@
+"""
+Why is this back tracking?
+Permutations are exhaustive. Since we need to walk back all paths 
+we can't really optimizae it.
+
+Approach here is DFS(Depth first search)
+we walk two paths one with lower case and other apper case
+
+        aA
+     bB    bB
+   cC cC  cC cC    
+
+Each leter reprsent edge on the graph not node
+"""
+import sys
+import os
+def dfs1(istr, i, res):
+    if i == len(istr):
+        res.append(''.join(istr))
+        return
+    #print(res)
+    istr[i] = istr[i].upper()
+    #print(istr)
+    dfs1(istr, i+1, res)
+    istr[i] = istr[i].lower()
+    dfs1(istr, i+1, res)
+
+def find_perm(res, inp):
+    """ Goa is to find all the permuations of the string passed"""
+    res = []
+    inp =["a","b","c"] 
+    indx = 0;
+    dfs1(inp, indx, res)
+    print(res)
+
+
+res=[]
+inp="abcd"
+ret = find_perm(res, inp)
